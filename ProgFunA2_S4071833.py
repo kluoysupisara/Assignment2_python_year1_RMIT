61556d4360b9f94f950ea6263889c2bd5ab5441b
Supisara Meemana <supisara.meemana@gmail.com>
2024-09-30 00:28:23 +1000

    Update ProgFunA2_S4071833.py
    Day2

--- ProgFunA2_S4071833.py
+++ ProgFunA2_S4071833.py
@@ -9,6 +9,7 @@ class Guest:
         self.reward = reward
         self.reward_rate = reward_rate
         self.redeem_rate = redeem_rate
+
     def get_reward(self, total_cost):
         self.reward = round(total_cost * (self.reward_rate/100))
 
@@ -18,10 +19,11 @@ class Guest:
         self.reward += new_reward
     
     def display_info(self):
-        print("guestID: {:<5}\tname: {:>10}\n\tthe reward_rate: {}\tthe reward: {}\n\tthe redeem_rate{}\n".format(self.ID, self.name, self.reward_rate, self.reward, self.redeem_rate))
+        print("guestID: {}\n\tname: {}\n\tthe reward_rate: {}\n\tthe reward: {}\n\tthe redeem_rate: {}\n".format(self.ID, self.name, self.reward_rate, self.reward, self.redeem_rate))
     
-    def set_reward_rate(self, new_value):
-        self.reward_rate = new_value
+    @staticmethod
+    def set_reward_rate(new_value):
+        Guest.reward_rate = new_value
     
     def set_redeem_rate(self, new_value):
         self.redeem_rate = new_value
@@ -40,8 +42,7 @@ class Product:
         return self.price
     
     def display_info(self):
-        print("print variable Productclass")
-        print("ID:{}\tname:{}\tprice:{}\n".format(self.ID, self.name, self.price))
+        print("ID:{}\t\nname:{}\t\nprice:{}\n".format(self.ID, self.name, self.price))
     
 class ApartmentUnit(Product):
     def __init__(self, ID, name, price, capacity):
@@ -56,12 +57,12 @@ class ApartmentUnit(Product):
     def get_capacity(self):
         return self.capacity
     def display_info(self):
-        print("ID:{}\tname:{}\tRate per Night:{}\ncapacity:{}".format(self.ID, self.name, self.price, self.capacity))
+        print("ID:{}\t\nname:{}\t\nRate per Night:{}\t\ncapacity:{}\n".format(self.ID, self.name, self.price, self.capacity))
 
 class SupplementaryItem(Product):
-    def __init__(self, ID, name, price, supplementaryItem):
+    def __init__(self, ID, name, price):
         super().__init__(ID, name, price)
-        self.supplementaryItem = supplementaryItem
+        #self.supplementaryItem = supplementaryItem
     
     def get_ID():
         return super().get_ID()
@@ -69,8 +70,7 @@ class SupplementaryItem(Product):
         return super().get_name()
     def get_price():
         return super().get_price()
-    def get_supplementaryItem(self):
-        return self.supplementaryItem
+
     def display_info(self):
         return super().display_info()
     
@@ -99,29 +99,37 @@ class Records:
     product_list = []
 
     def read_guests(self, filename):
-        file = open(filename,"r")
-        line = file.readline()
-        while line:
-            line_field = line.strip().split()
-            new_guest = Guest()
-            #format: guest_ID, guestname, reward rate,reward, and redeem rate. 
-            new_guest(line_field[0], line_field[1], line_field[2], line_field[3], line_field[4])
-            self.guest_list.append(new_guest)
+        try:
+            file = open(filename,"r")
             line = file.readline()
-        file.close()
+            while line:
+                line_field = line.strip().split(",")
+                #format: guest_ID, guestname, reward rate,reward, and redeem rate. 
+                new_guest = Guest(line_field[0], line_field[1], line_field[2], line_field[3], line_field[4])
+                self.guest_list.append(new_guest)
+                line = file.readline()
+            file.close()
+            return True
+        except FileNotFoundError:
+            return False
 
     def read_product(self, filename):
-        file = open(filename, "r")
-        line = file.readline()
-        while line:
-            line_field = line.strip().split()
-            if line_field[0].startswith("U"):
-                new_product = ApartmentUnit(line_field[0], line_field[1], line_field[2], line_field[3])
-            elif line_field[0] == "SI":
-                new_product = SupplementaryItem(line_field[0], line_field[1], line_field[2])
-            self.product_list.append(new_product)
+        try:
+            file = open(filename, "r")
             line = file.readline()
-        file.close()
+            while line:
+                line_field = line.strip().split(",")
+                if line_field[0].startswith("U"):
+                    new_product = ApartmentUnit(line_field[0], line_field[1], float(line_field[2]), int(line_field[3]))
+                    self.product_list.append(new_product)
+                elif line_field[0].startswith("SI"):
+                    new_product = SupplementaryItem(line_field[0], line_field[1], float(line_field[2]))
+                    self.product_list.append(new_product)
+                line = file.readline()
+            file.close()
+            return True
+        except FileNotFoundError:
+            return False
         
     def find_guest(self, search_value):
         for i in range(len(self.guest_list)):
@@ -144,32 +152,75 @@ class Records:
     def list_product(self, product_type):
         if product_type.lower() == "apartment":
             for apartment in self.product_list:
-                apartment.display_info()
+                if isinstance(apartment, ApartmentUnit):
+                    apartment.display_info()
         else:
-            for product in self.product_list:
-                product.display_info()
+            for si in self.product_list:
+                if isinstance(si, SupplementaryItem):
+                    si.display_info()
+
 # the main class of program
 class Operations:
-    record = Records()
-    record.read_guests("guest.csv")
-    record.read_product("product.csv")
+
+    def __init__(self, records):
+        self.records = records
+
+    def display_menu(self):
+        print("1. Make a booking")
+        print("2. Display existing guests")
+        print("3. Display existing apartment units")
+        print("4. Display existing supplementary items")
+        print("5. Exit")
+        menu = input("Select an menu: \n")
+        self.select_menu(menu)
+
+    def select_menu(self, menu):
+        if menu == '1':
+            self.make_booking()
+        elif menu == '2':
+            self.display_guests()
+        elif menu == '3':
+            self.display_apartments()
+        elif menu == '4':
+            self.display_supplementary_items()
+        elif menu == '5':
+            exit()
+        else:
+            print("Invalid menu, please try again.")
+            self.display_menu()
     
-    def make_a_booking():
+    def make_booking():
         
         print()
-    def display_existing_guest():
-        print()
+    def display_guests(self):
+        return self.records.list_guest()
 
-    def display_existing_apartment():
-        print()
-    def display_existing_supplementary_items():
-        print()
-
+    def display_apartments(self):
+        return self.records.list_product("apartment")
+    
+    def display_supplementary_items(self):
+        return self.records.list_product("SI")
+    
     def exit():
         print()
 
 
+        
 
 
+records = Records()
+guest_file = records.read_guests("guests.csv")
+product_file = records.read_product("products.csv")
 
+if not guest_file and not product_file:
+    print("Error both: guests.csv and products.csv are missing!!!")
+    exit()
+elif not guest_file:
+    print("Error guests.csv is missing!!!")
+elif not product_file:
+    print("Error products.csv is missing!!!")
+else:
+    print("Loaded both files successfully!!!!")
+    operations = Operations(records)
+    operations.display_menu()
 
